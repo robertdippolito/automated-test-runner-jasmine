6519f91ba66e5b4043a68c77cb099751effea02a
r.dippolito <robert.e.dippolito@gmail.com>
2020-04-23 12:11:51 -0400

    first commit

---
+++ spec/helpers/enzyme.js
@@ -0,0 +1,9 @@
+import jasminEnzyme from 'jasmine-enzyme';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() })
+
+beforeEach(() => {
+    jasminEnzyme()
+})
